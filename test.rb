28afd54a5936266b1ebeb00ebebdc5830f2dd40f
shen-sat <shensat18@gmail.com>
2018-03-22 06:18:54 +0000

    worked!

--- test.rb
+++ test.rb
@@ -15,15 +15,22 @@ client = Twitter::REST::Client.new do |config|
 end
 
 #collect followers
-followers = client.followers("HackneyPirates").take(1)
+followers = client.followers("HackneyPirates")
 
 #for each follower, list the name and bio
-n = 0
-followers.each do |follower|
-	sheet1[n,0] = "#{follower.name}"
-	sheet1[n,1] = "#{follower.description}"
-	sheet1[n,2] = "#{follower.followers_count}"
-	n += 1
+begin
+	n = 0
+	followers.each do |follower|
+		sheet1[n,0] = "#{follower.name}"
+		sheet1[n,1] = "#{follower.description}"
+		sheet1[n,2] = "#{follower.followers_count}"
+		n += 1
+	end
+rescue Twitter::Error::TooManyRequests => error
+  # NOTE: Your process could go to sleep for up to 15 minutes but if you
+  # retry any sooner, it will almost certainly fail with the same exception.
+  sleep error.rate_limit.reset_in + 1
+  retry
 end
 
 #write to file
